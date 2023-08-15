2e88658e11ac4d211c060694e3a783e79df801b2
Iweavetales <runeweaver2@gmail.com>
2023-08-16 02:53:35 +0900

    refactoring

---
+++ app/routes/about.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+//
+import { Outlet } from 'react-router';
+import { Link } from 'lunargate-test-helloworld3/router';
+// import { useServerFetches } from '../swift2/serverFetches';
+import LunarLogo from "../assets/svg/LunarLogo.svg"
+export default function IndexPage() {
+  // const serverFetches = useServerFetches();
+
+  return (
+    <div>
+        <img src={"/static/svg/LunarLogo.svg"} width={100}/>
+
+      LunarGate About
+      <Link href={'/'}>To Index</Link>
+      <Outlet />
+    </div>
+  );
+}
---
+++ lunar.conf.js
@@ -0,0 +1,11 @@
+/** @type {import('lunargate-test-helloworld3/config').default} */
+
+/**
+ *
+ * @type lunarConfig
+ */
+const lunarConfig = {
+    production: true
+}
+
+module.exports = lunarConfig
